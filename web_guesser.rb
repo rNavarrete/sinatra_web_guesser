b3b5332681952b089800482d30c9f0bb95976be5
Edgard Navarrete <kinglouis.belmont@gmail.com>
2014-09-02 23:35:22 -0600

    added colors to html background depending on guess

--- web_guesser.rb
+++ web_guesser.rb
@@ -7,18 +7,28 @@ end
 
 get '/' do
   session["number"] ||= rand(101)
-  guess               = params["guess"]
-  erb :index, :locals => {:message => check_message(session["number"], guess)}
+  number = session["number"]
+  erb :index, :locals => {:id => color_choice(number, params["guess"]), :message => check_message(number, params["guess"])}
 end
 
-def check_message(session_num, params)
-  if params.nil?
+def check_message(number, guess)
+  if guess.nil?
     "Hello Welcome to number guesser enter a guess"
-  elsif session_num > params.to_i
+  elsif number > guess.to_i
     "Hey your guess is too low. Try again."
-  elsif session_num < params.to_i
+  elsif number < guess.to_i
     "Your guess was too high try again."
   else
     "You got it! Congratulations."
+  end
+end
+
+def color_choice(number, guess)
+  if guess.nil?
+    "white"
+  elsif number > guess.to_i
+    "red"
+  elsif number < guess.to_i
+    "green"
   end
 end
