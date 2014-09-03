9df6ba00d9c3a3ec1fa38ee4ae76326d9b6421b6
Edgard Navarrete <kinglouis.belmont@gmail.com>
2014-09-02 21:05:46 -0600

    added a messages method

--- web_guesser.rb
+++ web_guesser.rb
@@ -7,6 +7,18 @@ end
 
 get '/' do
   session["number"] ||= rand(101)
-  erb :index #, :locals => {(session["number"]).to_sym => number}
+  guess               = params["guess"]
+  erb :index, :locals => {:message => check_message(session["number"], guess)}
 end
 
+def check_message(session_num, params)
+  if params.nil?
+    "Hello Welcome to number guesser enter a guess"
+  elsif session_num > params.to_i
+    "Hey your guess is too low. Try again."
+  elsif session_num < params.to_i
+    "Your guess was too high try again."
+  else
+    "You got it! Congratulations."
+  end
+end
